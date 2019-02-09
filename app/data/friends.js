b85d7bb250729b021093f572ff902656f5c73244
AmbesaLij <mikemulat@gmail.com>
2019-02-09 09:52:30 -0500

    Folder structure skeleton

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+// ***************************************
+// Friends survey data
+// ***************************************
+
+var friendSurveyData = [
+    {
+        name: "Michael",
+        photo: "",
+        scores: ["5","1","4","4","5","1","2","5","4","1"]
+    }
+]
+
+// Export the data 
+module.exports = friendSurveyData;
